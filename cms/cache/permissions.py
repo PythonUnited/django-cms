2b9e9984ada98d75e4ef93114a5f5f3bb90a5da3
Niels Siemons <niels@egoactive.com>
2013-06-17 21:13:25 +0200

    Nog wat User imports aangeapst

--- cms/cache/permissions.py
+++ cms/cache/permissions.py
@@ -3,7 +3,8 @@ from cms.utils import get_cms_setting
 from django.conf import settings
 from django.core.cache import cache
 
-from django.contrib.auth.models import User
+from cms.compat import User
+#from django.contrib.auth.models import User
 
 PERMISSION_KEYS = [
     'can_change', 'can_add', 'can_delete',
